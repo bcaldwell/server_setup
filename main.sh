060e4c063274beac1a99ff7d33e4797b056b03c9
Benjamin Caldwell <benjamincaldwell@users.noreply.github.com>
2015-11-30 23:54:20 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh\
 
-sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get install -y build-essential curl vim tmux git
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get install -y build-essential curl vim git
 
 if [ ! -f ~/.dotfiles ]; then
     git clone https://github.com/benjamincaldwell/.dotfiles "${ZDOTDIR:-$HOME}/.dotfiles"
@@ -12,9 +12,9 @@ make all
 cd ~/.dotfiles/vim
 make all
 
-cd ~/.dotfiles/tmux
-make all
+cd "$(dirname "$0")"
+sh tmux.sh
+
 
 cd "$(dirname "$0")"
-
 sh zsh.sh
