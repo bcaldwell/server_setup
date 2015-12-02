24064eb429b8570a724c7baf6fcc9e0939b0c66d
Benjamin Caldwell <benjamincaldwell@users.noreply.github.com>
2015-12-01 20:03:30 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,15 +6,10 @@ if [ ! -f ~/.dotfiles ]; then
     git clone https://github.com/benjamincaldwell/.dotfiles "${ZDOTDIR:-$HOME}/.dotfiles"
 fi
 
-cd ~/.dotfiles/git
-make all
+(cd ~/.dotfiles/git && make all)
 
-cd ~/.dotfiles/vim
-make all
+(cd ~/.dotfiles/vim && make all)
 
-cd "$(dirname "$0")"
 sh tmux.sh
 
-
-cd "$(dirname "$0")"
 sh zsh.sh
